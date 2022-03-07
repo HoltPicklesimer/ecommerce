9e08a83aec501d03ee8a3997e011a113b31d24b7
Holt Picklesimer <holtpicklesimer@gmail.com>
2022-03-07 17:32:37 -0600

    Add redux store

---
+++ backend/data.js
@@ -0,0 +1,42 @@
+const data = {
+   products: [
+      {
+         _id: '1',
+         name: 'Paper Flowers',
+         image: '/images/p1.jpg',
+         category: 'Flower',
+         price: 120,
+         countInStock: 10,
+         brand: 'Hanne Jensen',
+         rating: 4.5,
+         numReviews: 10,
+         description: 'High Quality Paper Flowers!',
+      },
+      {
+         _id: '2',
+         name: 'Paper Flower Boquet',
+         image: '/images/p2.jpg',
+         category: 'Boquet',
+         price: 300,
+         countInStock: 5,
+         brand: 'Hanne Jensen',
+         rating: 5,
+         numReviews: 15,
+         description: 'High Quality Paper Flowers!',
+      },
+      {
+         _id: '3',
+         name: 'Paper Rose',
+         image: '/images/p3.jfif',
+         category: 'Flower',
+         price: 200,
+         countInStock: 0,
+         brand: 'Hanne Jensen',
+         rating: 4,
+         numReviews: 20,
+         description: 'High Quality Paper Flowers!',
+      },
+   ],
+};
+
+export default data;
---
+++ backend/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import data from './data.js';
+
+const app = express();
+const port = process.env.port || 5000;
+
+app.get('/api/products', (req, res) => {
+   res.send(data.products);
+});
+
+app.get('/', (req, res) => {
+   res.send('Server is ready.');
+});
+
+app.listen(port, () => {
+   console.log(`Served at http://localhost:${port}`);
+});
